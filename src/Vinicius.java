6d2f4748b2f422fb5b220ec2c5252991e3fe7247
Vinicius Matias de Lima <vinicius.matias@labs.dcx>
2022-11-21 09:53:19 -0300

    Add class

---
+++ src/Vinicius.java
@@ -0,0 +1,12 @@
+public class Vinicius {
+    private int numero1;
+    private int numero2;
+    private int resultado;
+
+    public int somar(int num1, int num2){
+        numero1 = num1;
+        numero2 = num2;
+        resultado = numero1+numero2;
+        return resultado;
+    }
+}
